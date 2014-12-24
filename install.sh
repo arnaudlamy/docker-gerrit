1831c41ddf5beff4ee54293e99d895fa7793269e
Arnaud <arnaud.lamy@gmail.com>
2014-12-24 14:28:42 +0100

    fix re install issue

--- install.sh
+++ install.sh
@@ -4,5 +4,6 @@ mkdir -p log/supervisor
 mkdir -p git
 sudo pip install fig
 fig rm
+find ./log -type f -delete
 fig build
 fig run gerrit java -jar /home/gerrit/gerrit.war init -d /home/gerrit/gerrit
